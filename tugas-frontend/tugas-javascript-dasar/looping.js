ac71c4e8a38ac0026e1165d34fd80d2865aa12a0
satriadhapa <satriadhapa10@gmail.com>
2024-07-10 05:49:10 +0700

    fetch: create soal looping

---
+++ tugas-frontend/tugas-javascript-dasar/looping.js
@@ -0,0 +1,16 @@
+// Soal Looping
+// Jawaban Looping While
+
+console.log("LOOPING PERTAMA");
+var i = 2;
+while (i <= 20) {
+    console.log(i + " - I love coding so much");
+    i += 2;
+}
+
+console.log("LOOPING KEDUA");
+var j = 20;
+while (j >= 2) {
+    console.log(j + " - I will become a laravel developer and data scientist");
+    j -= 2;
+}
