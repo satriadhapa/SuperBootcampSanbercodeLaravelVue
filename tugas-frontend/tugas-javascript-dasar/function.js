a7b762466cf440ee49df064b0dcae51cca1608be
satriadhapa <satriadhapa10@gmail.com>
2024-07-10 05:34:44 +0700

    fetch: create soal function

---
+++ tugas-frontend/tugas-javascript-dasar/function.js
@@ -0,0 +1,24 @@
+// Soal Function
+// Jawaban Soal 1
+function teriak(){
+    return "Halo Sanber!!!";
+}
+
+console.log(teriak());
+
+// Jawaban Soal 2
+function kalikan(num1, num2){
+    return num1 * num2
+}
+
+console.log(kalikan(4,5))
+
+
+// Jawaban Soal 3
+
+function introduce(name, age, address, hobby){
+    return "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobi yaitu " + hobby;
+}
+
+console.log(introduce("satria", 22, "kota sukabumi", "mendengarkan musik"))
+
