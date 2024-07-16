e7def138f1eb6965111c74edc1399e040702d903
satriadhapa <satriadhapa10@gmail.com>
2024-07-16 23:19:25 +0700

    fetch : add footer

--- Mini_Project_2/portfolio/src/App.vue
+++ Mini_Project_2/portfolio/src/App.vue
@@ -2,11 +2,13 @@
   <div id="app">
     <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import Header from './components/header.vue';
+import Footer from './components/footer.vue';
 </script>
 
 <style>
