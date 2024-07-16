2516c7517c370cbadbed1139860115c13473ce80
satriadhapa <satriadhapa10@gmail.com>
2024-07-17 00:02:20 +0700

    fetch : modify homeView

--- Mini_Project_2/portfolio/src/views/HomeView.vue
+++ Mini_Project_2/portfolio/src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="container mx-auto p-4">
-    <h1 class="text-3xl font-bold mb-4">Welcome to My Portfolio</h1>
-    <p>This is the home page content.</p>
+  <div class="container mx-auto p-6 text-center animate__animated animate__fadeIn">
+    <img src="@/assets/profile.jpg" alt="Profile" class="w-32 h-32 rounded-full mx-auto mb-4">
+    <h1 class="text-4xl font-bold mb-4 text-gray-800">Welcome to My Portfolio</h1>
+    <p class="text-lg text-gray-600">Hello! I am Moch Satria Dhapa Hamdani, a passionate data science and web development.</p>
   </div>
 </template>
 
@@ -9,4 +10,5 @@
 </script>
 
 <style scoped>
+@import 'animate.css';
 </style>
