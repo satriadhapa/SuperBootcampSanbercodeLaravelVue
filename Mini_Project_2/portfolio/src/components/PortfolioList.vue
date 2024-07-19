bf569a981894f0a6d445a811a82f92f29d4fd593
satriadhapa <satriadhapa10@gmail.com>
2024-07-19 08:42:05 +0700

    fetch: modify portfoliolist

--- Mini_Project_2/portfolio/src/components/PortfolioList.vue
+++ Mini_Project_2/portfolio/src/components/PortfolioList.vue
@@ -14,33 +14,34 @@
 <script setup>
 import { ref } from 'vue';
 import project1 from '@/assets/datascience.jpg';
-import project2 from '@/assets/analis.png';
-import project3 from '@/assets/analisis.jpg';
+import project2 from '@/assets/data analis project.png';
+import project3 from '@/assets/postman.png';
+import project4 from '@/assets/Customer Churn Prediction.png';
 
 const portfolioItems = ref([
   { 
-    name: 'Project Data Science', 
-    description: 'Description of Project 1', 
-    link: '#',
+    name: 'Model Machine Learning', 
+    description: 'This machine learning model aims to classify hate speech in the Twitter comments column using the Long Short Term Memory algorithm. In testing this model, deployment was carried out using Gradio', 
+    link: 'https://github.com/satriadhapa/Model-Machine-Learning',
     image: project1
   },
   { 
-    name: 'Project Data Analis', 
-    description: 'Description of Project 2', 
-    link: '#',
+    name: 'Laporan Bencana Alam dan kerusakan Ekonomi', 
+    description: 'dashboard is a visualization tool that presents information related to natural disasters and their economic impacts. This dashboard may include data such as the number of natural disasters that have occurred, the location of the disaster, the economic losses caused, as well as possible factors causing disasters such as earthquakes', 
+    link: 'https://lookerstudio.google.com/u/0/reporting/014bc9c6-0d37-4d70-93a1-bb7fc6eeefdc/page/DxDhD',
     image: project2
   },
   { 
-    name: 'Project Web Development', 
-    description: 'Description of Project 3', 
-    link: '#',
+    name: 'Project Web Development Laravel', 
+    description: 'This film review project is a postman documentation in which there is backend logic that runs on a film review application.', 
+    link: 'https://documenter.getpostman.com/view/36502041/2sA3XY6dfE',
     image: project3
   },
   { 
-    name: 'Project ', 
-    description: 'Description of Project 3', 
-    link: '#',
-    image: project3
+    name: 'Model Machine Learning Customer Churn', 
+    description: 'This machine learning model is the result of practical work carried out at the Achmad Zaky Foundation. This machine learning model aims to predict customer churn based on the length of time they shop and how much customers spend on a particular item', 
+    link: 'https://github.com/satriadhapa/Model-Machine-Learning/blob/master/Project%20KP.ipynb',
+    image: project4
   },
 ]);
 </script>
