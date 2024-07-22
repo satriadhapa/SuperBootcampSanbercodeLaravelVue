3e90b9954dc38674227ac3220af6fe83d08b405d
satriadhapa <satriadhapa10@gmail.com>
2024-07-22 22:20:10 +0700

    modify index.js"

--- BE-Laravel/FE-Review-Film/src/router/index.js
+++ BE-Laravel/FE-Review-Film/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 import VerifyAccount from '@/views/VerifyAccount.vue';
