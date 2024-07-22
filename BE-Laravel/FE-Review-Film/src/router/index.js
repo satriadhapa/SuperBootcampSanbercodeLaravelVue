86aa220bc6e357fc6d5e315ccd3a5d27d1e2c488
satriadhapa <satriadhapa10@gmail.com>
2024-07-22 21:45:05 +0700

    fetch: create film view

--- BE-Laravel/FE-Review-Film/src/router/index.js
+++ BE-Laravel/FE-Review-Film/src/router/index.js
@@ -3,7 +3,7 @@ import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 import VerifyAccount from '@/views/VerifyAccount.vue';
-// import Film from './views/Film.vue';
+import Film from '@/views/Film.vue';
 // import Genre from './views/Genre.vue';
 // import Cast from './views/Cast.vue';
 
@@ -12,6 +12,7 @@ const routes = [
   { path: '/login', component: Login },
   { path: '/register', name: 'Register', component: Register },
   { path: '/verify-account', name: 'VerifyAccount', component: VerifyAccount },
+  { path: '/film', name: 'Film', component: Film },
   
 ];
 
@@ -26,6 +27,6 @@ export default router;
 // { path: '/login', name: 'Login', component: Login },
 //   { path: '/register', name: 'Register', component: Register },
 //   
-//   { path: '/film', name: 'Film', component: Film },
+//   
 //   { path: '/genre', name: 'Genre', component: Genre },
 //   { path: '/cast', name: 'Cast', component: Cast }
---
+++ BE-Laravel/FE-Review-Film/src/views/Film.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="min-h-screen bg-gray-100 p-4">
+      <div class="container mx-auto p-4">
+        <h1 class="text-3xl font-bold mb-6 text-center">Film List</h1>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <FilmCard
+            v-for="(film, index) in films"
+            :key="index"
+            :film="film"
+          />
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import FilmCard from '@/components/FilmCard.vue';
+  import poster1 from '@/assets/minion.png';
+  import poster2 from '@/assets/avenger.png';
+  import poster3 from '@/assets/satuhari.png';
+  import poster4 from '@/assets/serdam.jpg';
+  
+  const films = ref([
+    {
+      title: 'Minion',
+      description: 'Deskripsi singkat tentang Film A',
+      poster: poster1,
+    },
+    {
+      title: 'avenger',
+      description: 'Perjuangan Superhero',
+      poster: poster2,
+    },
+    {
+      title: 'satu hari',
+      description: 'berbeda dalam 1 hari',
+      poster: poster3,
+    },
+    {
+      title: 'serdam',
+      description: 'the death whistle',
+      poster: poster4,
+    },
+    
+  ]);
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
